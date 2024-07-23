70f7cb706a64ea3247a6f5c8c262cace2f7abf07
William Jeremy James <143300889+Theseus88@users.noreply.github.com>
2024-07-22 23:14:30 -0500

    Update Image Training Dataset.cpp
    Added EMNIST Image And Label Extraction

--- Image Training Dataset.cpp
+++ Image Training Dataset.cpp
@@ -1,17 +1,37 @@
 // Custom Libraries
 #include "Neural Networks.h"
+#include "EMNIST.h"
 
 // Functions
+void exampleEMNISTExtraction(void);
 void exampleImageTrainingDataset(void);
 
 // Program Entry Point
 int main()
 {
+    // Example Extracting EMNIST Images And Labels
+    exampleEMNISTExtraction();
+
     // Example Using Image Training Dataset Class
     exampleImageTrainingDataset();
 
     // Exit Program
     return 0;
+}
+
+void exampleEMNISTExtraction(void)
+{
+    // Local Variables Used For The EMNIST Extraction - Replace Paths With Your Own Paths
+    std::string filePathTrainingImages = "C:\\Users\\Theseus88\\OneDrive\\Desktop\\train-images.idx3-ubyte"; // 60,000 images
+    std::string filePathTrainingLabels = "C:\\Users\\Theseus88\\OneDrive\\Desktop\\train-labels.idx1-ubyte"; // 60,000 image labels
+    std::string filePathTestImages = "C:\\Users\\Theseus88\\OneDrive\\Desktop\\t10k-images.idx3-ubyte"; // 10,000 images
+    std::string filePathTestLabels = "C:\\Users\\Theseus88\\OneDrive\\Desktop\\t10k-labels.idx1-ubyte"; // 10,000 labels
+    std::string directoryImages = "C:\\Users\\Theseus88\\OneDrive\\Desktop\\EMNIST MNIST Database\\Images\\";
+    std::string directoryImageAnnotations = directoryImages + "Image Annotations\\";
+
+    // Extract 70,000 EMNIST MNIST Images And Labels
+    extractEMNISTImagesAndLabels(filePathTrainingImages, filePathTrainingLabels, directoryImages, directoryImageAnnotations, 0);
+    extractEMNISTImagesAndLabels(filePathTestImages, filePathTestLabels, directoryImages, directoryImageAnnotations, 60000);
 }
 
 void exampleImageTrainingDataset(void)
@@ -21,8 +41,8 @@ void exampleImageTrainingDataset(void)
     std::string datasetPath = "C:\\Users\\Theseus88\\OneDrive\\Desktop\\Image Dataset 0000";
     std::string trainingSession = "Session 0000";
     std::string fileStemArchive = "Archive 0000";
-    double neuralNetworkEta = 0.15; // Learning Rate ( 0 < value <= 1 )
-    double neuralNetworkAlpha = 0.5; // Momentum ( 0 < value <= 1 )
+    double neuralNetworkEta = 0.15; // Learning Rate ( 0 < value >= 1 )
+    double neuralNetworkAlpha = 0.5; // Momentum ( 0 < value >= 1 )
     std::vector<unsigned> neuralNetworkHiddenLayersTopology = { 8, 4, 2 };
     double percentTrainingData = 0.75;
     double percentValidationData = 0.20;
